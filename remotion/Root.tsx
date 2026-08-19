9a378aad49bdae4d3ef838f884f2a47af0145baa
hcz19950202-beep <hcz19950202@gmail.com>
2026-08-20 01:23:56 +0800

    fix: use inferred Remotion composition props

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -1,1 +1,21 @@
-import React from "react";import {Composition} from "remotion";import {MasterComposition,type MasterCompositionProps} from "./MasterComposition";import {createProject} from "@/lib/project/factory";const sample=createProject({id:"render-default",name:"Render Default",durationInFrames:30});export const RemotionRoot=()=> <Composition<MasterCompositionProps> id="VideoOSMaster" component={MasterComposition} width={1080} height={1920} fps={30} durationInFrames={30} defaultProps={{project:sample,assetUrls:{},renderMode:"final"}}/>;
+import { Composition } from "remotion";
+import { createProject } from "@/lib/project/factory";
+import { MasterComposition } from "./MasterComposition";
+
+const sample = createProject({
+  id: "render-default",
+  name: "Render Default",
+  durationInFrames: 30,
+});
+
+export const RemotionRoot = () => (
+  <Composition
+    id="VideoOSMaster"
+    component={MasterComposition}
+    width={1080}
+    height={1920}
+    fps={30}
+    durationInFrames={30}
+    defaultProps={{ project: sample, assetUrls: {}, renderMode: "final" }}
+  />
+);
